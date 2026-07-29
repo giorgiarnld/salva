f3cd5260ececec7d701d77627167da4d0a1d3734
giorgiarnld <giorgiarnld@proton.me>
2026-07-29 17:10:43 +0300

    Create menu.py

---
+++ src/cli/menu.py
@@ -0,0 +1,10 @@
+def show_menu():
+
+    print()
+
+    print("1. Add Book")
+    print("2. Update Progress")
+    print("3. Library")
+    print("4. Statistics")
+    print("5. Export")
+    print("6. Exit")
